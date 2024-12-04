e65fd1fe79983f6915f244c494a775ad035084fc
vbs925 <varunsb004@gmail.com>
2024-12-04 11:39:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Change navbar style on scroll
+window.addEventListener("scroll", () => {
+    const navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
